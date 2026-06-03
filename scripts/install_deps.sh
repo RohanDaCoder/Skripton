dccb374e4fb7b668d2dfb2cce5a27db3a324cf43
RohanDaCoder <rohanbmgo2010@gmail.com>
2026-06-03 19:16:29 +0600

    refactor(scripts): silence install_deps script

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,9 +1,9 @@
 %%bash
-curl -LsSf https://astral.sh/uv/install.sh | sh
+curl -LsSf https://astral.sh/uv/install.sh | sh &> /dev/null
 echo "⏳ Installing Skripton dependencies for Google Colab..."
-uv pip install -U torchao 
-uv pip install --no-deps "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git" 
-uv pip install unsloth_zoo 
-uv pip install --no-deps bitsandbytes xformers==0.0.27.post2 datasets 
+uv pip install -U torchao &> /dev/null
+uv pip install --no-deps "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git" &> /dev/null
+uv pip install unsloth_zoo &> /dev/null
+uv pip install --no-deps bitsandbytes xformers==0.0.27.post2 datasets &> /dev/null
 
 echo "✅ All dependencies installed successfully!"
