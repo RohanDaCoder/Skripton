ef1e76ed5729aca6f0e3ea4ecbf0678912cf4c45
RohanDaCoder <rohanbmgo2010@gmail.com>
2026-06-03 19:43:41 +0600

    fix(deps): install correct packages

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,9 +1,10 @@
 %%bash
 curl -LsSf https://astral.sh/uv/install.sh | sh &> /dev/null
 echo "⏳ Installing Skripton dependencies for Google Colab..."
-uv pip install -U torchao &> /dev/null
-uv pip install --no-deps "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git" &> /dev/null
-uv pip install unsloth_zoo &> /dev/null
-uv pip install --no-deps bitsandbytes xformers==0.0.27.post2 datasets &> /dev/null
+uv pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git" &> /dev/null
+uv pip install "transformers>=4.46.0,<4.50.0" &> /dev/null
+uv pip install --no-deps unsloth_zoo &> /dev/null
+uv pip install --no-deps bitsandbytes xformers==0.0.27.post2 &> /dev/null
+uv pip install peft accelerate trl datasets &> /dev/null
 
 echo "✅ All dependencies installed successfully!"
